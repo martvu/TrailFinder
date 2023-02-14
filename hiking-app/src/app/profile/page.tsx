66d9d542ba4d2db77b6f0e306a1d92cea167e001
kristrap <kristrap@stud.ntntu.no>
2023-02-14 15:15:26 +0100

    endret på utseendet

--- hiking-app/src/app/profile/page.tsx
+++ hiking-app/src/app/profile/page.tsx
@@ -6,38 +6,47 @@ export default function Profile() {
     return (
         <>
             <Header />
-
             <div className="flex w-full">
-      <div className="w-4/5 p-4">
-        <div className="flex justify-between items-center mb-4">
-          
-          <div>
-            <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded">
-              Edit Profile
-            </button>
-          </div>
+  <div className="w-4/5 p-4">
+    <div className="flex items-center mb-4 font-family: 'Open Sans', sans-serif;">
+      <img src="bruker.png" alt="User icon" className="w-32 h-32 mr-4" />
+      <div>
+        <h1 className="text-2xl font-bold">Kristine Eide Rapp</h1>
+        <div className="mb-4">
+          <p className="text-lg">krapp</p>
         </div>
         <div className="mb-4">
-          <h2 className="text-lg font-bold">Username:</h2>
-          <p>myusername</p>
+          <p className="text-lg">kristrap@gmail.com</p>
         </div>
-        <div className="mb-4">
-          <h2 className="text-lg font-bold">Email:</h2>
-          <p>user@example.com</p>
+        <div className="flex items-center">
+          <a href="#" className="text-gray-700 text-sm underline">Edit Profile</a>
         </div>
-       
-      </div>
-
-
-      <div className="w-1/5 p-4">
-        <h2 className="text-lg font-bold mb-4">Sidebar Content</h2>
-        <ul>
-          <li>Link 1</li>
-          <li>Link 2</li>
-          <li>Link 3</li>
-        </ul>
       </div>
     </div>
-</>
+  </div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                <div className="w-1/5 p-4">
+                    <h2 className="text-lg font-bold mb-4">Sidebar Content</h2>
+                    <ul>
+                        <li>Link 1</li>
+                        <li>Link 2</li>
+                        <li>Link 3</li>
+                    </ul>
+                </div>
+            </div>
+        </>
     );
 }
