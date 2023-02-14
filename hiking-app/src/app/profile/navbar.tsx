fbfb0b1435e9d1b9f22d0fbee4293cb004bcaf9a
kristrap <kristrap@stud.ntntu.no>
2023-02-14 15:35:12 +0100

    feat: create_ profile_sceleton

--- hiking-app/src/app/profile/navbar.tsx
+++ hiking-app/src/app/profile/navbar.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 
-const Pictures = () => {
+const Navbar = () => {
   return (
-    <div className="w-1/5 p-4">
+    <div className="w-4/5 p-4">
         divdAPA-stilen er brukt innenfor samfunnsfag og humaniora, og til dels innenfor helsefag. Du bør undersøke hvilken stil instituttet ditt anbefaler før du begynner å skrive oppgaven.
     </div>
   );
 };
 
-export default Pictures;
+export default Navbar;
--- hiking-app/src/app/profile/page.tsx
+++ hiking-app/src/app/profile/page.tsx
@@ -1,45 +1,36 @@
 import React from "react"
 import Image from "next/image"
 import Header from "../login/header";
+import Pictures from "./pictures";
+import Navbar from "./navbar";
 
 export default function Profile() {
     return (
         <>
-            <Header />
+         <Header />
             <div className="flex w-full">
-  <div className="w-4/5 p-4">
-    <div className="flex items-center mb-4 font-family: 'Open Sans', sans-serif;">
-      <img src="bruker.png" alt="User icon" className="w-32 h-32 mr-4" />
-      <div>
-        <h1 className="text-2xl font-bold">Kristine Eide Rapp</h1>
-        <div className="mb-4">
-          <p className="text-lg">krapp</p>
-        </div>
-        <div className="mb-4">
-          <p className="text-lg">kristrap@gmail.com</p>
-        </div>
-        <div className="flex items-center">
-          <a href="#" className="text-gray-700 text-sm underline">Edit Profile</a>
-        </div>
-      </div>
-    </div>
-  </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
+                <div className="w-4/5 p-4">
+                    <div className="flex items-center mb-4 font-family: 'Open Sans', sans-serif;">
+                        <img src="bruker.png" alt="User icon" className="w-32 h-32 mr-4" />
+                        <div>
+                            <h1 className="text-2xl font-bold">Kristine Eide Rapp</h1>
+                            <div className="mb-4">
+                                <p className="text-lg">krapp</p>
+                            </div>
+                            <div className="mb-4">
+                                <p className="text-lg">kristrap@gmail.com</p>
+                            </div>
+                            <div className="flex items-center">
+                                <a href="#" className="text-gray-700 text-sm underline">Edit Profile</a>
+                            </div>
+                        </div>
+                    </div>
+                    <Navbar />
+                    <Pictures />
+                </div>
 
                 <div className="w-1/5 p-4">
-                    <h2 className="text-lg font-bold mb-4">Sidebar Content</h2>
+                    <h2 className="text-lg font-bold mb-4">ANNONSER</h2>
                     <ul>
                         <li>Link 1</li>
                         <li>Link 2</li>
