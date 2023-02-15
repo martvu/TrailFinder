ec8792719dda6836cf6ddbaa28c18ba5fd70f0cc
ROversand <96338299+ROversand@users.noreply.github.com>
2023-02-15 11:41:41 +0100

    fix: add_daisyUI_theme

--- hiking-app/tailwind.config.js
+++ hiking-app/tailwind.config.js
@@ -1,8 +1,35 @@
+const themes = require('daisyui/src/colors/themes')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{js,ts,jsx,tsx}", "node_modules/daisyui/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
+  },
+  daisyui: {
+    themes: [
+      { mytheme: {
+          
+        "primary": "#40332e",
+                 
+        "secondary": "#a1df50",
+                 
+        "accent": "#cba8a3",
+                 
+        "neutral": "#f9f7eb",
+                 
+        "base-100": "#f5f5f4",
+                 
+        "info": "#2563EB",
+                 
+        "success": "#16A34A",
+                 
+        "warning": "#D97706",
+                 
+        "error": "#DC2626",
+                 },
+               },
+    ],
   },
   plugins: [require("daisyui")],
   daisyui: {
