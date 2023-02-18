081551ab8cbab3f9030ce6cb127758f4ee895655
Kristoffer Leyton <kriss.1999@hotmail.com>
2023-02-18 11:04:10 +0100

    Signup connected to firebase

--- hiking-app/src/app/login/components/login.tsx
+++ hiking-app/src/app/login/components/login.tsx
@@ -3,58 +3,63 @@ import { FirebaseError } from "firebase/app";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import Link from "next/link";
 import { useState } from "react";
-import { Navigate, useNavigate } from "react-router-dom";
+import { useRouter } from "next/navigation";
 import InputField from "../signup/components/inputfield";
 
 export default function Login() {
-	const [error, setError] = useState("");
-	const [email, setEmail] = useState("");
-	const [password, setPassword] = useState("");
-	const navigate = useNavigate();
+  const [error, setError] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const router = useRouter();
 
-	function login() {
-		setError("");
-		signInWithEmailAndPassword(auth, email, password)
-			.then(() => {
-				navigate('/home')
-        console.log("Logged in")
-			})
-			.catch((e: FirebaseError) => {
-				setError(e.message);
-			});
-	}
-	return (
-		<div className="w-96 max-h p-6 shadow-lg bg-white rounded-lg">
-			<h1 className="text-3xl block text-center font-bold text-primary">
-				Login
-			</h1>
-			<InputField
-				label="Username"
-				type="text"
-				placeholder="Enter username..."
-        setInput={setEmail}
-			/>
-			<InputField
-				label="Password"
-				type="password"
-				placeholder="Enter password..."
-        setInput={setPassword}
-			/>
-			<div className="mt-5 flex justify-center">
-				<button
-					className="btn btn-primary text-white mt-5 flex justify-center rounded-md"
-					type="submit"
-					onClick={login}
-				>
-					Log in
-				</button>
-			</div>
-			<div className="mt-3 text-center text-sm text-gray-500">
-				No account?{" "}
-				<Link href="/login/signup" className="font-bold text-primary">
-					Sign-up
-				</Link>
-			</div>
-		</div>
-	);
+  function login() {
+    setError("");
+    signInWithEmailAndPassword(auth, email, password)
+      .then(() => {
+        console.log("Logged in");
+        router.push('/home');
+      })
+      .catch((e: FirebaseError) => {
+        setError(e.message);
+      });
+  }
+  return (
+    <div className="w-96 max-h p-6 shadow-lg bg-white rounded-lg">
+      <form onSubmit={(e) => {
+        e.preventDefault();
+        login();
+      }}>
+        <h1 className="text-3xl block text-center font-bold text-primary">
+          Login
+        </h1>
+        <InputField
+          label="Username"
+          type="text"
+          placeholder="Enter username..."
+          setInput={setEmail}
+        />
+        <InputField
+          label="Password"
+          type="password"
+          placeholder="Enter password..."
+          setInput={setPassword}
+        />
+        <div className="mt-5 flex justify-center">
+          <button
+            className="btn btn-primary text-white mt-5 flex justify-center rounded-md"
+            type="submit"
+          >
+            Log in
+          </button>
+        </div>
+      </form>
+
+      <div className="mt-3 text-center text-sm text-gray-500">
+        No account?{" "}
+        <Link href="/login/signup" className="font-bold text-primary">
+          Sign-up
+        </Link>
+      </div>
+    </div>
+  );
 }
--- hiking-app/src/app/login/signup/page.tsx
+++ hiking-app/src/app/login/signup/page.tsx
@@ -1,42 +1,42 @@
 'use client'
 
-import {createUserWithEmailAndPassword, signInWithEmailAndPassword} from 'firebase/auth';
-import React, {useState} from 'react';
-import {auth, database, firestore} from "src/app/firebase";
-import { setDoc, doc } from 'firebase/firestore';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
+import React, { useState } from 'react';
+import { auth, db, firestore } from "src/app/firebase";
+import { setDoc, doc, addDoc, collection, Timestamp } from 'firebase/firestore';
 import InputField from './components/inputfield';
-import { useNavigate } from 'react-router-dom';
+import { useRouter } from 'next/navigation';
+import { ref, set } from 'firebase/database';
 
 export default function Signup() {
-  //const navigate = useNavigate();
   const [firstName, setFirstName] = useState("");
   const [lastName, setLastName] = useState("");
   const [birthDate, setBirthDate] = useState("");
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [username, setUsername] = useState("");
   const [error, setError] = useState("");
-  const navigate = useNavigate();
+  const router = useRouter();
 
-  function signup() {
+  async function signup() {
     setError("");
-    createUserWithEmailAndPassword(auth, email, password)
-    .then(async (userCredential) => {
-        // Signed in 
-        const user = userCredential.user;
-        await setDoc(doc(firestore,"users", user.uid), {name: user.email})
-            .then((e) => console.log(e))
-            .catch((error) => console.log(error))
-            console.log('Document Added')
-        navigate('/login')
-    })
-    .catch((error) => {
-        const errorCode = error.code;
-        const errorMessage = error.message;
-        setError("Sign-up was not successful.. try again!")
-
-    });
+    try {
+      const userCredential = await createUserWithEmailAndPassword(auth, email, password);
+      const user = userCredential.user;
+      await setDoc(doc(firestore,"users", user.uid), {
+        firstname: firstName,
+        lastname: lastName,
+        birthdate: Timestamp.fromDate(new Date(birthDate)),
+        username: username,
+      });
+      set(ref(db, 'users/' + username), email );
+      router.push('/login');
+    } catch (e) {
+      // delete created documents from potentially auth, firestore and realtime database
+      console.log("Error: " + e);
+      setError("Sign-up was not successful.. try again!")
+    }
   }
-
   return (
     <div>
       <div className={"flex h-screen items-center justify-center"}>
@@ -46,14 +46,18 @@ export default function Signup() {
               Sign-up
             </h1>
           </div>
-          <InputField label="First Name" placeholder="Enter first name..." setInput={setFirstName} type="text" />
-          <InputField label="Last Name" placeholder="Enter last name..." setInput={setLastName} type="text" />
-          <InputField label="Date of Birth" placeholder="Enter ..." setInput={setBirthDate} type="date" />
-          <InputField label="E-mail" placeholder="Enter e-mail..." setInput={setEmail} type="email" />
-          <InputField label="Password" placeholder="Enter password..." setInput={setPassword} type="password" />
-          <div className='mt-3'>
-            <button className="btn btn-primary text-white font-bold py-2 px-4 rounded-md justify-center" type="submit" onClick={signup}>Sign-up</button>
-          </div>
+          <form onSubmit={(e) => {
+            e.preventDefault(); // prevent default form submission behavior
+            signup(); // call your signup function
+          }}>
+            <InputField label="First Name" placeholder="Enter first name..." setInput={setFirstName} type="text" />
+            <InputField label="Last Name" placeholder="Enter last name..." setInput={setLastName} type="text" />
+            <InputField label="Date of Birth" placeholder="Enter ..." setInput={setBirthDate} type="date" />
+            <InputField label="Username" placeholder="Enter username..." setInput={setUsername} type="text" />
+            <InputField label="E-mail" placeholder="Enter e-mail..." setInput={setEmail} type="email" />
+            <InputField label="Password" placeholder="Enter password..." setInput={setPassword} type="password" />
+            <button className="btn btn-primary text-white font-bold py-2 px-4 rounded-md justify-center" type="submit">Sign-up</button>
+          </form>
         </div>
       </div>
     </div>
