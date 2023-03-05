e125e7240c9678f96d017fca7c04381727d66e83
eriknd <eriknd@stud.ntnu.no>
2023-03-05 02:46:17 +0100

    Test airbnb setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -37,6 +37,27 @@ module.exports = {
 					'body': ['"Open Sans"'],
 				  }
 			},
+			{
+				trailfinder_light: {
+					"primary": "#1eb854",
+					"secondary": "#40332e",
+					"accent": "#cba8a3",
+					"neutral": "#f9f7eb",
+					"base-100": "#f5f5f5",
+					"info": "#2563EB",
+					"success": "#16A34A",
+					"warning": "#D97706",
+					"error": "#DC2626",
+						  },
+						  fontFamily: {
+							  'sans': ['ui-sans-serif', 'system-ui'],
+							  'serif': ['ui-serif', 'Georgia',],
+							  'mono': ['ui-monospace', 'SFMono-Regular'],
+							  'display': ['Oswald'],
+							  'body': ['"Open Sans"'],
+							}
+
+			}
 		],
 		base: true,
 		utils: true,
