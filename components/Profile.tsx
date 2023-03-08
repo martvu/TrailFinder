0477cd37f1d136e813836746f2e9a40a12dc1471
Martin Vu <martinvu@stud.ntnu.no>
2023-03-08 21:25:23 +0100

    fix: bug updating likes in profile page

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Header from "./Header";
 import 'firebase/firestore'
 import useFetchPosts from "hooks/fetchPosts";
@@ -10,27 +10,60 @@ import { PostData } from "hooks/PostData";
 
 export default function Profile({ setEdit }: any) {
   const { userData } = useFetchUser();
-  const { recentPostsList, likedPostsList, setLikedPostsList, usersPostsList, setUsersPostsList } = useFetchPosts();
+  const { recentPostsList, likedPostsList, usersPostsList } = useFetchPosts();
   //const usersList = useState(recentPostsList.filter(post => post.username == userData?.username));
   //const likedPosts = recentPostsList.filter(post => post.likedBy.includes(userData?.username)); 
   const [myPosts, setMyPosts] = useState(true);
-  const [liked, setLiked] = useState(false);
+  const [likedPosts, setLikedPosts] = useState(false);
+  const [myPostsArray, setMyPostsArray] = useState([] as PostData[]);
+  const [likedPostsArray, setLikedPostsArray] = useState([] as PostData[]);
+
+  useEffect(() => {
+    setLikedPostsArray(likedPostsList);
+    setMyPostsArray(usersPostsList)
+  }, [likedPostsList, usersPostsList]);
 
   function handleMyPostsClick() {
     setMyPosts(true);
-    setLiked(false);
+    setLikedPosts(false);
   }
 
   function handleLikedPostsClick() {
     setMyPosts(false);
-    setLiked(true);
-    console.log(liked)
+    setLikedPosts(true);
   }
 
-  function onLike(post: any) {
-    setTimeout(() => {
-      window.location.reload();
-    }, 1000);
+  /*Makes sure the likes update correctly in profile page */
+  function onLike(post: PostData) {
+
+    if (myPosts) {
+      if (post.likedBy.includes(userData.username)) {
+        const newLikedPostsList = likedPostsArray.filter((likedPost) => likedPost.id !== post.id);
+        setLikedPostsArray(newLikedPostsList);
+      } else {
+        const newLikedPostsList = likedPostsArray.concat(post);
+        setLikedPostsArray(newLikedPostsList);
+      }
+    }
+
+    if (likedPosts) {
+      if (post.likedBy.includes(userData.username)) {
+        const newLikedPostsList = likedPostsArray.filter((likedPost) => likedPost.id !== post.id);
+        setLikedPostsArray(newLikedPostsList);
+
+        const unLikePost = myPostsArray.find(oldPost => oldPost.id === post.id);
+        if (unLikePost) {
+          const userIndex = unLikePost.likedBy.indexOf(userData.username);
+          if (userIndex !== -1) unLikePost.likedBy.splice(userIndex, 1);
+          const postIndex = myPostsArray.findIndex(p => p.id === post.id);
+
+          if (postIndex !== -1) {
+            const updatedPosts = [...myPostsArray.slice(0, postIndex), unLikePost, ...myPostsArray.slice(postIndex + 1)];
+            setMyPostsArray(updatedPosts);
+          }
+        }
+      }
+    }
   }
   return (
     <>
@@ -82,25 +115,25 @@ export default function Profile({ setEdit }: any) {
           <div className="flex w-full flex-col sm:w-3/5">
             <div className="mt-3 mb-3 justify-between flex">
 
-              <button onClick={handleMyPostsClick} className={`${myPosts ? 'btn-primary text-lg text-white font-extrabold' : ''} 
+              <button onClick={handleMyPostsClick} className={`${myPosts ? 'btn-primary text-lg text-neutral font-extrabold' : ''} 
               btn flex-1 text-center p-3 justify-center font-bold cursor-pointer`}>
                 My posts  <i className="mx-2 mb-1 fa-solid fa-user"></i>
               </button>
 
-              <button onClick={handleLikedPostsClick} className={`${liked ? 'btn-accent text-lg text-white font-extrabold' : ''} 
+              <button onClick={handleLikedPostsClick} className={`${likedPosts ? 'btn-accent text-lg text-neutral font-extrabold' : ''} 
               btn flex-1 text-center p-3 font-bold cursor-pointer`}>
                 Liked posts <i className="mx-2 fa-solid fa-heart"></i>
               </button>
 
             </div>
 
-            {liked && likedPostsList.map((post) => (
+            {likedPosts && likedPostsArray.map((post) => (
               <div key={post.id} className="mb-2">
                 <PostCard post={post} onLike={() => onLike(post)} />
               </div>
             ))}
 
-            {myPosts && usersPostsList.map((post) => (
+            {myPosts && myPostsArray.map((post) => (
               <div key={post.id} className="mb-2">
                 <PostCard post={post} onLike={() => onLike(post)} />
               </div>
