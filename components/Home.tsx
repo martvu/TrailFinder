6a2427db77545548a3422ce9bcc65f522cee0379
Martin Vu <martinvu@stud.ntnu.no>
2023-03-14 09:01:04 +0100

    feat: fix lint and update look

--- components/Home.tsx
+++ components/Home.tsx
@@ -14,7 +14,6 @@ export default function Home() {
       <div className="flex flex-row justify-center">
         <CreatePostModal />
         {/* Main screen in the middle */}
-
         <div className="flex flex-col w-full min-w-[70%] sm:w-3/5 place-items-center p-2 gap-2">
           <div className="w-full main-box flex justify-between items-center">
             <div className="flex main-box w-full">
--- components/SortButtons.tsx
+++ components/SortButtons.tsx
@@ -1,61 +1,61 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-
-
-interface sortOption {
+interface SortOption {
   text: string;
   onClick?: () => void;
   isSelected: boolean;
 }
 
 export default function SortButtons() {
-  const [sortBy, setSortBy] = useState("Recent Posts")
+  const [sortBy, setSortBy] = useState('Recent Posts');
 
-  /**Sorting options */
-  const recommended: sortOption = {
-    text: "Recommended",
-    onClick: () => setSortBy("Recommended Posts"),
-    isSelected: sortBy === "Recommended Posts"
-  }
-  const mostLiked: sortOption = {
-    text: "Most Liked",
-    onClick: () => setSortBy("Most Liked"),
-    isSelected: sortBy === "Most Liked"
-  }
-  const recent: sortOption = {
-    text: "Recent",
-    onClick: () => setSortBy("Recent Posts"),
-    isSelected: sortBy === "Recent Posts"
-  }
-  const alphabetical: sortOption = {
-    text: "Alphabetical",
-    onClick: () => setSortBy("Alphabetical"),
-    isSelected: sortBy === "Alphabetical"
-  }
-  const sortOptions = [recommended, recent, mostLiked, alphabetical]
-
+  /* Sorting options */
+  const recommended: SortOption = {
+    text: 'Recommended',
+    onClick: () => setSortBy('Recommended Posts'),
+    isSelected: sortBy === 'Recommended Posts',
+  };
+  const mostLiked: SortOption = {
+    text: 'Most Liked',
+    onClick: () => setSortBy('Most Liked'),
+    isSelected: sortBy === 'Most Liked',
+  };
+  const recent: SortOption = {
+    text: 'Recent',
+    onClick: () => setSortBy('Recent Posts'),
+    isSelected: sortBy === 'Recent Posts',
+  };
+  const alphabetical: SortOption = {
+    text: 'Alphabetical',
+    onClick: () => setSortBy('Alphabetical'),
+    isSelected: sortBy === 'Alphabetical',
+  };
+  const sortOptions = [recommended, recent, mostLiked, alphabetical];
 
   return (
-    <>
-
-      <div className="px-2 flex items-center justify-between rounded-xl shadow-md min-w-full max-w-full h-12 bg-neutral">
-         {/** Sort by Button*/}
-        <div className=''>
-          {
-            <div className='flex flex-row z-10 rounded-lg'>
-              {sortOptions.map((item, i) => (
-                <div key={i} onClick={item.onClick} className= {`btn btn-xs btn-outline justify-start px-2 mr-2 ${item.isSelected ? 'btn-active' : ''}`} >
-                  <h3 className=''>{item.text}</h3>
-                </div>
-              ))}
+    <div className=" flex items-center justify-between min-w-full max-w-full h-12">
+      {/* Sort by Button */}
+      <div className="">
+        <div className="flex flex-row items-center z-10 rounded-lg">
+          <span className="pr-2 font-bold">Sort by: </span>
+          {sortOptions.map((item) => (
+            <div
+              key={item.text}
+              role="button"
+              tabIndex={0}
+              onClick={item.onClick}
+              onKeyDown={(e) => {
+                if ((e.key === 'Enter' || e.key === ' ') && item.onClick) {
+                  item.onClick();
+                }
+              }}
+              className={`btn btn-sm btn-outline btn-neutral justify-start px-2 mr-2 ${item.isSelected ? 'btn-active' : ''}`}
+            >
+              <h3 className="">{item.text}</h3>
             </div>
-          }
+          ))}
         </div>
-
       </div>
-    </>
-  )
+    </div>
+  );
 }
-
-
-
